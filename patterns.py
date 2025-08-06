0417a3062add16a2ec0e84849de667a3ada2c354
ThasmiyaThamim <thasmiyathamim82@gmail.com>
2025-08-06 21:13:13 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,51 +1,51 @@
-# n = int(input("enter a num:"))
-# for i in range(n):
-#     for j in range(n):
-#         print("#",end ="  ")
-#     print() for square
+n = int(input("enter a num:"))
+for i in range(n):
+    for j in range(n):
+        print("#",end ="  ")
+    print() for square
 n = int(input("enter a num:"))
 for i in range(n):
     for j in range(i+1):
         print("#",end ="  ")
     print()
 
-# n=int(input("num:"))
-# for i in range(1,n+1):
-#     print(" # "*i) for increaing pattern
+n=int(input("num:"))
+for i in range(1,n+1):
+    print(" # "*i) for increaing pattern
 
-# n = int(input("enter a num:"))
-# for i in range(n):
-#     for j in range(i,n):
-#         print("*",end ="  ")
-#     print()
+n = int(input("enter a num:"))
+for i in range(n):
+    for j in range(i,n):
+        print("*",end ="  ")
+    print()
 
-# n=int(input())
-# for i in range(n):
-#     for j in range(i,n):
-#         print(" ",end=' ')
-#     for j in range(i+1):
-#         print("*",end=" ")
-#     print()
+n=int(input())
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=' ')
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
 
 
-# n=int(input())
-# for i in range(n):
-#     for j in range(i+1):
-#         print(" ",end=" ")
-#     for j in range(i,n):
-#         print("* ",end=' ')
-#     print()
+n=int(input())
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("* ",end=' ')
+    print()
 
-#hill pattern
-# n=int(input())
-# for i in range(n):
-#     for j in range(i,n):
-#         print(" ",end=" ")
-#     for j in range(i+1):
-#         print("*",end=' ')
-#     for j in range(i):
-#         print("*",end= " ")
-#     print()
+hill pattern
+n=int(input())
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=' ')
+    for j in range(i):
+        print("*",end= " ")
+    print()
 
 n=int(input("enter a num:"))
 for i in range(n):
@@ -55,4 +55,5 @@ for i in range(n):
         print(" *",end=" ")
     for j in range(1,n):
         print(" *",end=" ")
+
     print()
